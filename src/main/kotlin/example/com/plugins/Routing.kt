5913c5d78f6c3ed0c69ba0f23cf685d98ec437db
ken205115331@gmail.com <ken205115331@gmail.com>
2024-07-25 08:49:15 +0000

    upload file

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -3,11 +3,38 @@ package example.com.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.http.content.*
+import io.ktor.server.request.*
+import java.io.File
 
+//https://ktor.io/docs/server-requests.html#form_data
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        var fileDescription = ""
+        var fileName = ""
+
+        post("/upload") {
+            val multipartData = call.receiveMultipart()
+
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        fileDescription = part.value
+                    }
+
+                    is PartData.FileItem -> {
+                        fileName = part.originalFileName as String
+                        val fileBytes = part.streamProvider().readBytes()
+                        File("data/uploads/$fileName").writeBytes(fileBytes)
+                    }
+
+                    else -> {}
+                }
+                part.dispose()
+            }
+
+            call.respondText("$fileName is uploaded to 'uploads/$fileName'")
         }
     }
 }
+
